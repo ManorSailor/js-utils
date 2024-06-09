61a3fb31b6900a6ccb67000a9c74757027181395
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2024-06-09 13:43:24 +0530

    js/100jsfunctions: Fix bigger index edge case in lastNRevert

--- js/100jsfunctions/7. lastNRevert/lastNRevert.js
+++ js/100jsfunctions/7. lastNRevert/lastNRevert.js
@@ -7,15 +7,21 @@
  * ```js
  * lastNRevert("T-shirts", 4) // stri
  * ```
+ * @example Returns the full reversed str when n > str.length or n is a negative integer
+ * ```js
+ * lastNRevert("Hello", 11) // olleH
+ * lastNRevert("Hello", -11) // olleH
+ * ```
  */
 function lastNRevert(str, n) {
   let revStr = "";
   const endChar = str.length - 1;
-  const nthChar = str.length - n;
+  const nthChar = Math.abs(n) <= str.length ? str.length - n : 0;
 
   for (let i = endChar; i >= nthChar; i--) {
-    revStr += str[i] ?? ""; // or str.charAt(i)
+    revStr += str[i]; // or str.charAt(i)
   }
+
   return revStr;
   //   return str.split("").slice(-n).reverse().join("");
 }
